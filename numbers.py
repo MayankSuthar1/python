75dcb6cdbd8bf27047953a918e6e1e60e7437668
MayankSuthar1 <mayank992456@gmail.com>
2023-07-13 11:28:19 +0530

    7/13/23

---
+++ numbers.py
@@ -0,0 +1,15 @@
+a = 2.0
+print(a)
+
+
+b = 2.5
+print(b+a)
+print((a+b)*2)
+
+
+print(3/2)
+
+def MathOp():
+    print("helllo")
+
+MathOp()
---
+++ sec.py
@@ -0,0 +1,11 @@
+def MathOp():
+
+    division = 3/2
+    flo_division = 3//2
+    modulus = 3%2
+    power = 3**2
+    mutliple = 3*2
+
+    return [division,flo_division,modulus,power,mutliple]
+
+print(MathOp())
